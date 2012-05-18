6d4c9db8c9f2d8755e98110039faa0279b1ecb1f
Chris LaRose <cjlarose@gmail.com>
2012-05-18 01:18:00 -0700

    Working with threads

--- MagicSquares.java
+++ MagicSquares.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MagicSquares {
@@ -5,6 +6,7 @@ public class MagicSquares {
 	int order;
 	int max;
 	int magic_constant;
+	int count_magic;
 	
 	public static void main(String[] args) {
 		if (args.length > 0) {
@@ -26,31 +28,81 @@ public class MagicSquares {
                 end[i] = obj.max - i;
             }
             
-            int[] current_permutation = begin;
-            int count_magic = 0;
+            //int[] current_permutation = begin;
+            long i = 0;
+            long end_i = MagicSquares.factorial(obj.max);
             
+            int num_threads = 16;
+            int chunk_size = 40320;
+            
+            ArrayList<Thread> threads = new ArrayList<Thread>();
             
             while (true) {
-            	MagicSquares.SquareMatrix m = obj.new SquareMatrix(current_permutation);
+            	
+            	long a = i;
+            	long b = i + chunk_size;
+            	if (b > end_i) {
+            		b = end_i;
+            	}
+            	
+            	if (threads.size() < num_threads) {
+            		Thread t = new Thread(obj.new MatrixThread(a,b));
+                	threads.add(t);
+                	t.run();
+            	} else {
+            		for (int j = 0; j < threads.size(); j++) {
+            			if (!threads.get(j).isAlive()) {
+            				threads.remove(j);
+            				Thread t = new Thread(obj.new MatrixThread(a,b));
+            				threads.add(j, t);
+            			}
+            		}
+            	}
+            	
+            	if (i >= end_i)
+            		break;
+            	
+            	i += chunk_size + 1;
+            	
+            	/*MagicSquares.SquareMatrix m = obj.new SquareMatrix(current_permutation);
             	if (m.is_magic()) {
             		count_magic++;
             		System.out.println(m.toString());
             	}
             	if (Arrays.equals(current_permutation, end))
             		break;
-            	MagicSquares.nextPermutation(current_permutation);
+            	MagicSquares.nextPermutation(current_permutation);*/
             }
             
 			long end_time = System.currentTimeMillis();
             long runtime = end_time - start_time;
             //System.out.println(runtime);
             double runtime_seconds = (double) runtime / (double)1000;
-            System.out.println("Found "+count_magic+" magic squares in "+String.format("%f", runtime_seconds)+" seconds");
+            System.out.println("Found "+obj.count_magic+" magic squares in "+String.format("%f", runtime_seconds)+" seconds");
             //System.out.println("Found "+count_magic+" magic squares");
            
         } else {
             System.out.println("Usage: java MagicSquares <order>");
         }
+	}
+	
+	public class MatrixThread extends Thread {
+		long a;
+		long b;
+		public MatrixThread(long a, long b) {
+			this.a = a;
+			this.b = b;
+		}
+		public void run() {
+			for (long i = this.a; i < this.b; i++) {
+				int[] current_permutation = MagicSquares.getPermutation(max, i);
+				MagicSquares.SquareMatrix m = new SquareMatrix(current_permutation);
+	        	if (m.is_magic()) {
+	        		count_magic++;
+	        		System.out.println(m.toString());
+	        	}
+			}
+		}
 	}
 	
 	public class SquareMatrix {
@@ -158,6 +210,28 @@ public class MagicSquares {
 			result += str;
 		}
 		return result;
+	}
+	
+	public static long factorial(int n) {
+		long ret = 1;
+        for (int i = 1; i <= n; ++i) ret *= i;
+        return ret;
+    }
+	
+	public static int[] getPermutation(int n, long i) {
+		ArrayList<Integer> p = new ArrayList<Integer>();
+		for (int k = 0; k < n; k++) {
+			p.add(k+1);
+		}
+		
+		int[] r = new int[n];
+		for (int j = 0; j<n; j++) {
+			long g = MagicSquares.factorial(p.size() - 1);
+			r[j] = p.get((int)(i/g));
+			p.remove((int)(i/g));
+			i = i % g;
+		}
+		return r;
 	}
 
 	public static void nextPermutation(int[] digits) {
--- MagicSquaresTest.java
+++ MagicSquaresTest.java
@@ -10,7 +10,7 @@ public class MagicSquaresTest {
 		int[] digits = {3,6,2,5,4,1};
 		int[] new_digits = {3,6,4,1,2,5};
 		MagicSquares.nextPermutation(digits);
-		assertArrayEquals(new_digits, new_digits);
+		assertArrayEquals(digits, new_digits);
 	}
 	
 	@Test
@@ -18,7 +18,37 @@ public class MagicSquaresTest {
 		int[] digits = {1,2,3};
 		int[] new_digits = {1,3,2};
 		MagicSquares.nextPermutation(digits);
-		assertArrayEquals(new_digits, new_digits);
+		assertArrayEquals(digits, new_digits);
+	}
+	
+	@Test
+	public void testFactorial3() {
+		int n = 3;
+		assertEquals(MagicSquares.factorial(n), 6);
+	}
+	
+	@Test
+	public void testFactorial4() {
+		int n = 4;
+		assertEquals((int) MagicSquares.factorial(n),24);
+	}
+	
+	@Test
+	public void testFactorial16() {
+		int n = 16;
+		assertEquals(MagicSquares.factorial(n), 20922789888000L);
+	}
+	
+	@Test
+	public void testGetPermutation() {
+		int[] result0 = MagicSquares.getPermutation(4, 0L);
+		assertArrayEquals(result0, new int[] {1,2,3,4});
+		
+		int[] result2 = MagicSquares.getPermutation(4, 5L);
+		assertArrayEquals(result2, new int[] {1,4,3,2});
+		
+		int[] result23 = MagicSquares.getPermutation(4, 23L);
+		assertArrayEquals(result23, new int[] {4,3,2,1});
 	}
 	
 	@Test
