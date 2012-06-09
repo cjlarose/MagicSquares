05c6433c6d6d8fbf341f6105d5f7ebb79448b5dd
Chris LaRose <cjlarose@gmail.com>
2012-06-08 23:27:10 -0700

    Removed unused imports

--- MagicSquares.java
+++ MagicSquares.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class MagicSquares {
 	
